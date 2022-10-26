c7338e0af41f9b24581f60718cf0433b5c20ae7f
LsaacIee <leejihak02@gmail.com>
2022-10-26 12:46:21 +0900

    random numbered array code (need to fix)

--- 1.c
+++ 1.c
@@ -2,3 +2,19 @@
 #include <time.h>
 #define MAX 50
 
+void fillArray(int arr[MAX]){
+    srand(time(NULL));
+    for (int i = 0; i < MAX; i++){
+        arr[i] = rand()%1000;
+    }
+}
+
+int main(){
+    int* arr1[MAX];
+    fillArray(arr1[MAX]);
+    for (int i = 0; i < MAX; i++){
+        printf("%d ", arr1[i]);
+    }
+
+    return 0;
+}
