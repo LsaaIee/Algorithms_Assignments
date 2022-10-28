4c92477292fc3be470a860dee1cee4e094377bc7
LsaacIee <leejihak02@gmail.com>
2022-10-28 10:10:41 +0900

    hashFunction2 and 3 has problem

--- 2.c
+++ 2.c
@@ -1,4 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAX 50
+#define MAX 30
+#define LENGTH 37
+
+void print(int arr[], int size){
+    for (int i = 0; i < size; i++){
+        printf("%d ", arr[i]);
+    }
+}
+
+void hashFunc1(int arr[MAX]){
+    int temparr[LENGTH]; //hash table
+    int linearProbing = 0; //(h'k+i)%LENGTH
+    int probeCount1 = 0;
+    
+    for (int i = 0; i < LENGTH; i++){
+        temparr[i] = -1;
+    }
+
+    for (int j = 0; j < MAX; j++){
+        int result = arr[j]%LENGTH; //h'(k) = k%m
+        int k = 0;
+        while (temparr[(result+k)%LENGTH] != -1){
+            k++;
+            probeCount1++;
+        }
+        linearProbing = (result+k)%LENGTH;
+        temparr[linearProbing] = arr[j];
+        probeCount1++;
+    }
+    print(temparr, LENGTH);
+    printf("\n%d\n", probeCount1);
+}
+/*
+void hashFunc2(int arr[MAX]){
+    int temparr[LENGTH];
+    int probeCount2 = 0;
+    int quadraticProbing = 0;
+    
+    for (int i = 0; i < LENGTH; i++){
+        temparr[i] = -1;
+    }
+    for (int j = 0; j < MAX; j++){
+        int result = arr[j]%LENGTH;
+        int k = 0;
+        while (temparr[(result+j+3*j*j)%LENGTH] != -1){
+            k++;
+            probeCount2++;
+        }
+        quadraticProbing = (result+k+3*k*k)%LENGTH;
+        temparr[quadraticProbing] = arr[j];
+        probeCount2++;
+    }
+    print(temparr, LENGTH);
+    printf("\n%d\n", probeCount2);
+}
+*/
+void hashFunc3(int arr[MAX]){
+    int temparr[LENGTH];
+    int doubleHashing = 0;
+    int probeCount3 = 0;
+    
+    for (int i = 0; i < LENGTH; i++){
+        temparr[i] = -1;
+    }
+    for (int j = 0; j < MAX; j++){
+        int result1 = arr[j]%LENGTH;
+        int result2 = 1+(arr[j]%(LENGTH+1));
+        int k = 0;
+        while (temparr[(result1+k*result2)%LENGTH] != -1){
+            k++;
+            probeCount3++;
+        }
+        doubleHashing = (result1+k*result2)%LENGTH;
+        temparr[doubleHashing] = arr[j];
+        probeCount3++;
+    }
+    print(temparr, LENGTH);
+    printf("\n%d\n", probeCount3);
+}
+
+int main(){
+    //int temp[MAX];
+    int temp[MAX] = {382, 524, 291, 915, 80, 104, 106, 120, 667, 485, 612, 845, 913, 41, 921, 570, 533, 396, 482, 350, 618, 52, 227, 546, 379, 265, 801, 391, 461, 278}; //main array with keys
+    int n;
+    /*srand(time(NULL));
+    for(int i = 0; i < MAX; i++){
+        temp[i] = rand()%1000;
+        for (int j = 0; j < i-1; j++){
+            if (temp[j] == temp[i]){
+                i--;
+                break;
+            }
+        }
+    }*/
+    print(temp, MAX);
+    printf("\n\n");
+    hashFunc1(temp);
+    printf("\n\n");
+    //hashFunc2(temp);
+    //printf("\n\n");
+    hashFunc3(temp);
+    printf("\n\n");
+}
