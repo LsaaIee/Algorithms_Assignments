58090212d3d86956bd5b6b6f0afd59ed62861d13
LsaacIee <leejihak02@gmail.com>
2022-11-23 01:14:05 +0900

    Little bit of skeleton

--- report3_2021315317_이지학/main.c
+++ report3_2021315317_이지학/main.c
@@ -5,17 +5,39 @@
 #define NAME 10
 #define MAX 30
 
-int main(){
-    char name[NAME], source[2], dest[2], temp[MAX];
-    int date, i = 0;
+void input(char name[], char source[], char dest[], int date){
+    int i = 0;
+    while (i != 11){
+        printf("Input your name, source, destination, and date in order: \n");
+        while (scanf("%10[^,], %1[^,], %1[^,], %d", name, source, dest, &date) == 4){
+            printf("%s\n", name);
+            printf("%s\n", source);
+            printf("%s\n", dest);
+            printf("%d\n", date);
+            printf("----------------------------------------------------------------------------------\n");
+            rbPrint();
+            rbInsert();
+            break;
+        }
+        i++;
+    }
+}
 
-    while (scanf("%10[^,], %1[^,], %1[^,], %d", name, source, dest, &date) == 4){
-        printf("%s\n", name);
-        printf("%s\n", source);
-        printf("%s\n", dest);
-        printf("%d", date);
-        break;
-    }    
+void rbInsert(){
+
+}
+void rbDelete(){
+
+}
+void rbPrint(){
+
+}
+
+int main(){
+    char name[NAME], source[2], dest[2];
+    int date; 
+
+    input(name, source, dest, date);
     
     return 0;
 }
