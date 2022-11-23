a0f1078d4713acaddb95645d16ce128e72fc9d2d
LsaacIee <leejihak02@gmail.com>
2022-11-23 13:12:51 +0900

    InsertFixup done

--- report3_2021315317_이지학/main.c
+++ report3_2021315317_이지학/main.c
@@ -75,8 +75,60 @@ void rightRotate(struct node **root, struct node *y){
     y->parent = x;
 }
 
-void rbInsert(){
+void insertFixup(struct node **root, struct node *z){
+    while (z != *root && z->parent->color == 'R'){
+        struct node *y;
+        if (z->parent == z->parent->parent->left){
+            y = z->parent->parent->right;
+        }
+        else {
+            y = z->parent->parent->left;
+        }
 
+        if (y->color == 'R'){
+            y->color = 'B';
+            z->parent->color = 'B';
+            z->parent->parent->color = 'R';
+            z = z->parent->parent;
+        }
+        else {
+            if (z->parent == z->parent->parent->left && z == z->parent->left){
+                char ch = z->parent->color;
+                z->parent->color = z->parent->parent->color;
+                z->parent->parent->color = ch;
+                rightRotate(root, z->parent->parent);
+            }
+
+            if (z->parent == z->parent->parent->left && z == z->parent->right){
+                char ch = z->color ;
+                z->color = z->parent->parent->color;
+                z->parent->parent->color = ch;
+                leftRotate(root,z->parent);
+                rightRotate(root,z->parent->parent);
+            }
+
+            if (z->parent == z->parent->parent->right && z == z->parent->right)
+            {
+                char ch = z->parent->color ;
+                z->parent->color = z->parent->parent->color;
+                z->parent->parent->color = ch;
+                leftRotate(root,z->parent->parent);
+            }
+
+            if (z->parent == z->parent->parent->right && z == z->parent->left)
+            {
+                char ch = z->color ;
+                z->color = z->parent->parent->color;
+                z->parent->parent->color = ch;
+                rightRotate(root,z->parent);
+                leftRotate(root,z->parent->parent);
+            }
+        }
+    }
+    (*root)->color = 'B';
+}
+void rbInsert(struct node **root, int data){
+    
 }
 void rbDelete(){
 
